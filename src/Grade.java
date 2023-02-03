ecefdd87351a051d97d46bcda18381178973ecc6
91112021 <dalebolido@gmail.com>
2023-02-03 12:59:35 -0800

    Initial commit

---
+++ src/Grade.java
@@ -0,0 +1,12 @@
+public class Grade
+{
+    private String letter;
+    private int cutoff;
+
+    public Grade(String letter, int cutoff)
+    {
+        this.letter = letter;
+        this.cutoff = cutoff;
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args)
+    {
+
+        String[] grades = {"A", "A-", "B+", "B", "B-", "C+", "C", "C-", "D+", "D", "D-", "F"};
+        int[] cutoff = {95, 90, 87, 83, 80, 77, 73, 70, 67, 63, 60, 0};
+
+        Grade A = new Grade("A", 95);
+        Grade A2 = new Grade("A-", 90);
+        Grade B = new Grade("B+", 87);
+        Grade B2 = new Grade("B", 83);
+        Grade B3 = new Grade("B-", 80);
+        Grade C = new Grade("C+")
+
+
+    }
+}
